39a352e3d9bc4a7ea6634851b86df8f926b956dc
Fernando Dias Motta <fernando.motta@fit-tecnologia.org.br>
2021-03-16 00:00:29 -0300

    Criação do layout de datalhes

--- app/src/main/java/br/org/fittecnologia/fakenews/MainActivity.kt
+++ app/src/main/java/br/org/fittecnologia/fakenews/MainActivity.kt
@@ -2,10 +2,22 @@ package br.org.fittecnologia.fakenews
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import com.bumptech.glide.Glide
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        val imageView = findViewById<ImageView>(R.id.imagem_principal)
+
+        Glide
+            .with(this)
+            .load("https://ibcdn.canaltech.com.br/8V8CJ88HfAZgQvC9G8eKJPkYpXY=/368x25:1392x602/512x288/smart/i404557.png")
+            .placeholder(R.drawable.ic_noimage)
+            .into(imageView)
+
     }
 }
